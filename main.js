916a17f7b7859301d460d63a5422396673234914
unknown <berdibekovsamatxann@gmail.com>
2022-12-05 18:06:35 +0600

    first commit

--- main.js
+++ main.js
@@ -1,173 +1,91 @@
-const API = "http://localhost:8000/comments";
+let API = "http://localhost:8001/students";
 
-let inpName = document.getElementById("inp-Name");
-let inpEmail = document.getElementById("inp-Email");
-let inpNumbers = document.getElementById("inp-Numbers");
-let inpImgUrl = document.getElementById("inp-ImgUrl");
-
-let btnAdd = document.getElementById("btn");
-// console.log(btnAdd);
-
-btnAdd.addEventListener("click", async () => {
-  let newTodo = {
-    Name: inpName.value,
-    Email: inpEmail.value,
-    Numbers: inpNumbers.value,
-    ImgUrl: inpImgUrl.value,
-  };
-
-  // console.log(newTodo);
-
-  if (inpName.value.trim() == "") {
-    alert("Заполните имя!");
-    return;
-  } else if (inpEmail.value.trim() == "") {
-    alert("Заполните фамилию");
-    return;
-  } else if (inpNumbers.value.trim() == "") {
-    alert("Заполните номер");
-    return;
-  } else if (inpImgUrl.value.trim() == "") {
-    alert("Заполните ссылку на картинку!");
+//  ! инпуты для добавления // start
+let inpAdd_name = document.querySelector(".section__add_name");
+let inpAdd_surname = document.querySelector(".section__add_surname");
+let inpAdd_number = document.querySelector(".section__add_number");
+let inpAdd_KPI_week = document.querySelector(".section__add_KPI-week");
+let inpAdd_KPI_month = document.querySelector(".section__add_KPI-month");
+let btnAdd = document.querySelector(".student__add_save");
+let table = document.querySelector("table");
+let limit = 3;
+let inpabc = document.querySelector(".abc");
+// ! добавление в API
+btnAdd.addEventListener("click", () => {
+  // ! проверка на заполненность полей
+  if (
+    !inpAdd_name.value.trim() ||
+    !inpAdd_surname.value.trim() ||
+    !inpAdd_number.value.trim() ||
+    !inpAdd_KPI_week.value.trim() ||
+    !inpAdd_KPI_month.value.trim()
+  ) {
+    alert("заполните поле");
     return;
   }
+  let obj = {
+    name: inpAdd_name.value,
+    surname: inpAdd_surname.value,
+    number: inpAdd_number.value,
+    week: inpAdd_KPI_week.value,
+    month: inpAdd_KPI_month.value,
+  };
+  // console.log(obj);
+  createProduct(obj);
+  inpAdd_name.value = "";
+  inpAdd_surname.value = "";
+  inpAdd_number.value = "";
+  inpAdd_KPI_week.value = "";
+  inpAdd_KPI_month.value = "";
+});
 
-  await fetch(API, {
-    method: "post",
-    body: JSON.stringify(newTodo),
-    headers: {
-      "content-type": "application/json; charset=utf-8",
-    },
-  });
-  inpName.value = "";
-  inpEmail.value = "";
-  inpNumbers.value = "";
-  inpImgUrl.value = "";
-  localStorage.getItem(
-    newTodo.Name,
-    newTodo.Email,
-    newTodo.Numbers,
-    newTodo.ImgUrl
+let inpSearch = document.querySelector(".search-txt");
+
+// console.log(inpSearch.value);
+let seachValue = inpSearch.value;
+console.log(inpSearch);
+
+async function getAPI() {
+  let studentInfo = await fetch(`${API}?q=${seachValue}`).then((res) =>
+    res.json()
   );
-
-  getTodos();
-});
-
-let list = document.getElementById("list");
-
-getTodos();
-// console.log(list)
-
-async function getTodos() {
-  let responce = await fetch(API).then((res) => res.json());
-  list.innerHTML = "";
-  responce.forEach((item) => {
-    let newElem = document.createElement("div");
-    newElem.id = item.id;
-    newElem.innerHTML = `
-          <span>${item.Name}</span>
-          <span>${item.Email}</span>
-          <span>${item.Numbers}</span>
-          <img src="${item.ImgUrl}"></img>
-          <button class="btn-delete">delete</button>
-          <button class="btn-edit">edit</button>
-          `;
-    list.append(newElem);
+  studentInfo.forEach((info) => {
+    let trAdd = document.createElement("tr");
+    trAdd.innerHTML += `
+        <td>${info.name}</td>
+        <td>${info.surname}</td>
+        <td>${info.number}</td>
+        <td>${info.week}</td>
+        <td>${info.month}</td>`;
+    table.append(trAdd);
+    // console.log(info);
   });
+  // ! =============== SEARCH START ===============
+  // console.log(inpSearch.value);
+  inpSearch.addEventListener("input", (e) => {
+    seachValue = e.target.value;
+  });
+  getAPI();
+  // ? =============== SEARCH END =====================
+
+  let countPage = 1;
+  async function pageTotal() {
+    let data = await fetch(`${API}?q=${seachValue}`).then((res) => res.json());
+    // console.log(data.length);
+    countPage = Math.ceil(data.length / limit);
+  }
+  pageTotal();
 }
+// console.log(pageTotal());
+getAPI();
 
-document.addEventListener("click", async (e) => {
-  // console.log(e.target);
-  if (e.target.className === "btn-delete") {
-    let id = e.target.parentNode.id;
-    await fetch(`${API}/${id}`, {
-      method: "DELETE",
-    });
-    getTodos();
-  }
-  //   ! Update
-  if (e.target.className === "btn-edit") {
-    modalEdit.style.display = "flex";
-    let id = e.target.parentNode.id;
-    let responce = await fetch(`${API}/${id}`)
-      .then((res) => res.json())
-      .catch((err) => console.log(err));
-    getTodos();
-    // console.log(responce);
-    inpEditName.value = responce.Name;
-    inpEditEmail.value = responce.Email;
-    inpEditNumber.value = responce.Number;
-    inpEditImgUrl.value = responce.ImgUrl;
-    inpEditname.className = responce.id;
-    inpEditEmail.className = responce.id;
-    inpEditNumber.className = responce.id;
-    inpEditImgUrl.className = responce.id;
-  }
-});
-
-let modalEdit = document.querySelector("#modal-edit");
-
-let modalEditClose = document.querySelector("#modal-edit-close");
-
-let inpEditName = document.querySelector("#inp-edit-Name");
-
-let inpEditEmail = document.querySelector("#inp-edit-Email");
-
-let inpEditNumber = document.querySelector("#inp-edit-Number");
-
-let inpEditImgUrl = document.querySelector("#inp-edit-ImgUrl");
-
-let inpEditId = document.querySelector("#inp-edit-id");
-
-let btnsaveEdit = document.querySelector("#modal-edit-close");
-
-// console.log(modalEdit,modalEditClose,inpEditId,inpEditTodo);
-
-modalEditClose.addEventListener("click", () => {
-  modalEdit.style.display = "none";
-});
-
-btnsaveEdit.addEventListener("click", async () => {
-  let editedTodo = {
-    Name: inpEditName.value,
-    Email: inpEditEmail.value,
-    Number: inpEditNumber.value,
-    ImgUrl: inpEditImgUrl.value,
-  };
-  let id = inpEditName.className;
-  let id1 = inpEditEmail.className;
-  let id2 = inpEditNumber.className;
-  let id3 = inpEditImgUrl.className;
-  console.log(id);
-  console.log(editedTodo);
-  await fetch(`${API}/${id}`, {
-    method: "PATCH",
-    body: JSON.stringify(editedTodo),
+function createProduct(obj) {
+  fetch(API, {
+    method: "POST",
     headers: {
       "Content-type": "application/json; charset=utf-8",
     },
-  });
-  await fetch(`${API}/${id1}`, {
-    method: "PATCH",
-    body: JSON.stringify(editedTodo),
-    headers: {
-      "Content-type": "application/json; charset=utf-8",
-    },
-  });
-  await fetch(`${API}/${id2}`, {
-    method: "PATCH",
-    body: JSON.stringify(editedTodo),
-    headers: {
-      "Content-type": "application/json; charset=utf-8",
-    },
-  });
-  await fetch(`${API}/${id3}`, {
-    method: "PATCH",
-    body: JSON.stringify(editedTodo),
-    headers: {
-      "Content-type": "application/json; charset=utf-8",
-    },
-  });
-  modalEdit.style.display = "none";
-  getTodos();
-});
+    body: JSON.stringify(obj),
+  }).then((res) => res.json());
+  getAPI();
+}
